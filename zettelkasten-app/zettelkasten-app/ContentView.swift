085392a82ee4bb2cab36ccd32f1387c9810802ab
Tatsuya Sakurai <s.tatsuya0123@gmail.com>
2026-07-05 15:08:57 +0900

    chore: プロジェクトを作成

---
+++ zettelkasten-app/zettelkasten-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zettelkasten-app
+//
+//  Created by Tatsuya Sakurai on 2026/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zettelkasten-app/zettelkasten-app/zettelkasten_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  zettelkasten_appApp.swift
+//  zettelkasten-app
+//
+//  Created by Tatsuya Sakurai on 2026/07/04.
+//
+
+import SwiftUI
+
+@main
+struct zettelkasten_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
